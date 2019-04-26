062d71836a2ac633747aea6c13b14f4a0225c7a7
Davud <davud@comp.fritz.box>
2019-04-26 03:20:30 +0300

    bug fixed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 
 import Post from './entities/Post';
+import { continueStatement } from 'babel-types';
 
 
 const app = new Express();
@@ -22,16 +23,23 @@ app.get('/posts', (req, res) => {
   res.render('posts/index', { posts });
 });
 
+app.get('/posts/new', (req, res) => {
+  res.render('posts/new');
+});
+
 app.get('/posts/:id', (req, res) => {
   const id = req.params.id;
   const post = posts.filter(post => post.id === +id)[0];
   res.render('posts/show', { post, id });
 });
 
-app.get('/posts/new', (req, res) => {
-  res.render('posts/new');
+app.post('/posts', (req, res) => {
+  const { title, body } = req.body;
+  const newPost = new Post(title, body);
+  posts.push(newPost);
+  const id = newPost.id;
+  res.redirect(`/posts/${id}`);
 });
-
 
 export default app;
 
